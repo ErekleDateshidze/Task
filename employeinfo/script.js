5f38bcd742f9121bbbeaaf900fc273b8f1b4e8e5
Erekle99 <erekledateshidze99@gmail.com>
2022-09-01 17:23:25 +0400

    EmployeInfo Page
    API fetching teams and positions

--- employeinfo/script.js
+++ employeinfo/script.js
@@ -8,24 +8,56 @@ getTeams(API_URL)
 async function getTeams(url) {
     const res = await fetch(url)
     const data = await res.json()
-    // showTeams(data.results)
-    console.log(data)
+    showTeams(data)
+}
+
+
+function showTeams(teams) {
+    select.innerHTML = ''
+    teams.data.forEach((team) => {
+        const { name } = team
+
+        const teamEl = document.createElement('option')
+        teamEl.classList.add('team-option')
+
+        teamEl.innerHTML = `
+        <option> ${name}</option>
+        `
+
+        select.appendChild(teamEl)
+    });
 }
 
 
 
-// function showTeams(teams) {
-//     select.innerHTML = ''
-//     teams.forEach((team) => {
-//         const { name } = team
+const API_URL2 = 'https://pcfy.redberryinternship.ge/api/positions'
 
-//         const teamEl = document.createElement('option')
-//         teamEl.classList.add('team-option')
+const select2 = document.getElementById('select-position')
 
-//         teamEl.innerHTML = `
-//         <option> ${name}</option>
-//         `
+getPositions(API_URL2)
 
-//         select.appendChild(teamEl)
-//     });
-// }
+
+
+async function getPositions(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+    showPositions(data)
+}
+
+function showPositions(positions) {
+    select.innerHTML = ''
+    positions.data.forEach((position) => {
+
+        const { name } = position
+
+        const posEl = document.createElement('option')
+        posEl.classList.add('position-option')
+
+        posEl.innerHTML = `
+        <option> ${name}</option>
+        `
+
+        select.appendChild(posEl)
+    })
+}
+
