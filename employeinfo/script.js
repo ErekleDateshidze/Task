ca57c309ed90c77ca541bd8b4008c31ead3726b0
Erekle99 <erekledateshidze99@gmail.com>
2022-09-03 21:45:26 +0400

    emplye info page
    input validations

--- employeinfo/script.js
+++ employeinfo/script.js
@@ -1,8 +1,12 @@
 const API_URL = 'https://pcfy.redberryinternship.ge/api/teams'
+const API_URL2 = 'https://pcfy.redberryinternship.ge/api/positions'
 
-const select = document.getElementById('select-team')
+const selectTeam = document.getElementById('select-team')
+const selectPosition = document.getElementById('select-position');
+let allPositions;
 
 getTeams(API_URL)
+getPositions(API_URL2)
 
 
 async function getTeams(url) {
@@ -11,53 +15,132 @@ async function getTeams(url) {
     showTeams(data)
 }
 
-
 function showTeams(teams) {
-    select.innerHTML = ''
     teams.data.forEach((team) => {
         const { name } = team
 
-        const teamEl = document.createElement('option')
-        teamEl.classList.add('team-option')
-
-        teamEl.innerHTML = `
-        <option> ${name}</option>
+        selectTeam.innerHTML += `
+        <option class="teams" id="team-${team.id}" value="${team.id}"> ${name}</option>
         `
 
-        select.appendChild(teamEl)
     });
+
 }
 
+selectTeam.addEventListener('change', function (e) {
+    selectPosition.removeAttribute('disabled');
+    showPositions();
+});
 
+function showPositions() {
+    let positions = allPositions;
+    selectPosition.innerHTML = 'პოზიცია';
+    positions?.data.forEach(position => {
+        if (position.team_id === +selectTeam.value) {
+            const { name } = position
 
-// const API_URL2 = 'https://pcfy.redberryinternship.ge/api/positions'
+            selectPosition.innerHTML += `
+            <option> ${name}</option>
+            `
+        }
+    })
+}
 
-// const select2 = document.getElementById('select-position')
+async function getPositions(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+    allPositions = data
+    console.log(allPositions)
+}
 
-// getPositions(API_URL2)
+function validateNameAndSurname(fieldName) {
+    let field = document.getElementById(fieldName);
+    let fieldValidationMessage = document.getElementById('validation-' + fieldName);
+    let fieldRegex = (/^[ა-ჰ]+$/);
 
+    if (field.value.length <= 2 || field.value === '' || !field.value) {
+        fieldValidationMessage.innerHTML = 'მინიმუმ 2 სიმბოლო';
+        field.classList.add('validation-input')
+        fieldValidationMessage.classList.add('validation-text');
+        return false;
+    }
 
+    if (!field.value.match(fieldRegex)) {
+        fieldValidationMessage.innerHTML = 'გამოიყენე ქართული ასოები';
+        field.classList.add('validation-input')
+        fieldValidationMessage.classList.add('validation-text');
+        return false;
+    }
 
-// async function getPositions(url) {
-//     const res = await fetch(url)
-//     const data = await res.json()
-//     showPositions(data)
-// }
+    fieldValidationMessage.innerHTML = 'მინიმუმ 2 სიმბოლო, ქართული ასოები';
+    fieldValidationMessage.classList.remove('validation-text');
+    field.classList.remove('validation-input');
+    return true;
+}
 
-// function showPositions(positions) {
-//     select.innerHTML = ''
-//     positions.data.forEach((position) => {
+function validateTeam(fieldName) {
+    let field = document.getElementById(fieldName);
+    if (field.value === 'თიმი') {
+        field.classList.add('validation-input');
+        return false;
+    }
 
-//         const { name } = position
+    field.classList.remove('validation-input');
+    return true
+}
 
-//         const posEl = document.createElement('option')
-//         posEl.classList.add('position-option')
+function validateMail(fieldName) {
+    let field = document.getElementById(fieldName);
+    let fieldValidationMessage = document.getElementById('validation-' + fieldName);
+    let mail = field.value;
+    if (mail.search(/^[\w-\.]+@redberry.ge/) < 0) {
+        fieldValidationMessage.classList.add('validation-text');
+        field.classList.add('validation-input');
+        return false;
+    }
 
-//         posEl.innerHTML = `
-//         <option> ${name}</option>
-//         `
+    fieldValidationMessage.classList.remove('validation-text');
+    field.classList.remove('validation-input');
+    return true;
+}
 
-//         select.appendChild(posEl)
-//     })
-// }
+function validateTel(fieldName) {
+    let field = document.getElementById(fieldName);
+    let fieldValidationMessage = document.getElementById('validation-' + fieldName);
+    let numberRegex = /^\d+$/;
+    number = field.value.replace(/\s+/g, "");
+
+    if (number[0] != "+") {
+        fieldValidationMessage.innerHTML = 'მობილურის ნომერი უნდა იწყებოდეს "+"-ით';
+        fieldValidationMessage.classList.add('validation-text');
+        field.classList.add('validation-input');
+        return false
+    } else {
+        number = number.substring(1);
+    }
+
+    if (number.indexOf("9955") != 0 || numberRegex.test(number) != true || number.length != 12) {
+        fieldValidationMessage.innerHTML = 'უნდა აკმაყოფილებდეს ქართული მობ-ნომრის ფორმატს'
+        fieldValidationMessage.classList.add('validation-text');
+        field.classList.add('validation-input');
+        return false
+    }
+
+    fieldValidationMessage.classList.remove('validation-text');
+    field.classList.remove('validation-input');
+    return true;
+
+}
+
+function nextPage() {
+    validateNameAndSurname('name');
+    validateNameAndSurname('surname');
+    validateTeam('select-team');
+    validateMail('mail');
+    validateTel('tel')
+    let formValidationFailed = document.getElementsByClassName('validation-input').length
+    if (!formValidationFailed) {
+        // go to next page
+    }
+}
 
